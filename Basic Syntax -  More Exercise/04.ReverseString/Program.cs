f5df4d86f4e8cc20f37549c24eb4d58626b4bbac
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:39:32 +0300

    Create Program.cs

---
+++ Basic Syntax -  More Exercise/04.ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                char simbol = input[i];
+                Console.Write(simbol);
+            }
+        }
+    }
+}
