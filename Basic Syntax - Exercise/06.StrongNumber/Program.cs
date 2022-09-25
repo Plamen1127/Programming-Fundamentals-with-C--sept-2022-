71795fd81be57c68658fb8d2e22e9f5367916986
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:40:09 +0300

    Create Program.cs

---
+++ Basic Syntax - Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int total = 0;
+            int numCopy = num;
+            while (numCopy>0)
+            {
+                int lastDigit = numCopy % 10;
+                numCopy /= 10;
+
+                int sum = 1;
+                for (int i = 1; i <= lastDigit; i++)
+                {
+                    sum *= i;
+                }
+
+                total += sum;
+            }
+
+            if (total == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            
+
+       
+        }
+    }
+}
