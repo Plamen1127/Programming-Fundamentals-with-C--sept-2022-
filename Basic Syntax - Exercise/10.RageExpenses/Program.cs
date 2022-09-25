9f418f39f1d2df8e733d663da31fa999d9cbcc3b
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:40:43 +0300

    Create Program.cs

---
+++ Basic Syntax - Exercise/10.RageExpenses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGame = int.Parse(Console.ReadLine());
+            double priceHeadset = double.Parse(Console.ReadLine());
+            double priceMouse = double.Parse(Console.ReadLine());
+            double priceKeyboard = double.Parse(Console.ReadLine());
+            double priceDisplay = double.Parse(Console.ReadLine());
+
+            int qtyHeadset = lostGame / 2;
+            int qtyMouse = lostGame / 3;
+            int qtyKeyboard = lostGame / 6;
+            int qtyDisplay = lostGame / 12;
+
+            double moneyHeadset = qtyHeadset * priceHeadset;
+            double moneyMouse = qtyMouse * priceMouse;
+            double moneyKeyboard = qtyKeyboard * priceKeyboard;
+            double moneyDisplay = qtyDisplay * priceDisplay;
+
+            double allMoney = moneyHeadset + moneyMouse + moneyKeyboard + moneyDisplay;
+
+            Console.WriteLine($"Rage expenses: {allMoney:F2} lv.");
+        }
+    }
+}
