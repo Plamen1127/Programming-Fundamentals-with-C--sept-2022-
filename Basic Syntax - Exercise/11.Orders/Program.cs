ccee490e0641d005f76ae0c270001782bae8825f
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:40:56 +0300

    Create Program.cs

---
+++ Basic Syntax - Exercise/11.Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtyOrders = int.Parse(Console.ReadLine());
+            double totalSum = 0;
+            for (int i = 1; i <=qtyOrders; i++)
+            {
+                double allPrice = 0;
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int qtyCapsules = int.Parse(Console.ReadLine());
+                allPrice = (days * qtyCapsules) * pricePerCapsule;
+
+                Console.WriteLine($"The price for the coffee is: ${allPrice:F2}");
+                totalSum += allPrice;
+            }
+            Console.WriteLine($"Total: ${totalSum:F2}");
+        }
+    }
+}
