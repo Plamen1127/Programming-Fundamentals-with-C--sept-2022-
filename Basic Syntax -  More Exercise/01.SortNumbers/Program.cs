dff60db05c09e543c0704a30180c001b478a49b6
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:39:03 +0300

    Create Program.cs

---
+++ Basic Syntax -  More Exercise/01.SortNumbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _01.SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1 >= n2 && n1 >= n3)
+            {
+                Console.WriteLine(n1);
+            }
+            else if (n2 >= n1 && n2 >= n3)
+            {
+                Console.WriteLine(n2);
+            }
+            else if (n3 >= n1 && n3 >= n2)
+            {
+                Console.WriteLine(n3);
+            }
+            if (n1 >= n2 && n1 <= n3)
+            {
+                Console.WriteLine(n1);
+            }
+            else if (n1 <= n2 && n1 >= n3)
+            {
+                Console.WriteLine(n1);
+            }
+
+
+            else if (n2 >= n3 && n2 <= n1)
+            {
+                Console.WriteLine(n2);
+            }
+            else if (n2 <= n3 && n2 >= n1)
+            {
+                Console.WriteLine(n2);
+            }
+
+            else if (n3 >= n1 && n3 <= n2)
+            {
+                Console.WriteLine(n3);
+            }
+            else if (n3 <= n1 && n3 >= n2)
+            {
+                Console.WriteLine(n3);
+            }
+
+            if (n1 <= n2 && n1 <= n3)
+            {
+                Console.WriteLine(n1);
+            }
+            else if (n2 <= n3 && n2 <= n1)
+            {
+                Console.WriteLine(n2);
+            }
+            else if (n3 <= n1 && n3 <= n2)
+            {
+                Console.WriteLine(n3);
+            }
+        }
+
+    }
+}
