48ac23c0224042856344d986c65863ba2d41436d
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:40:27 +0300

    Create Program.cs

---
+++ Basic Syntax - Exercise/08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08.TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(type == "Equilateral")
+                {
+                    int qtySpeis = n - i;
+                    for (int speis = 1; speis <= qtySpeis; speis++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                for (int j = 1; j <=i; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
