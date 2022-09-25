1eb00e1c8f1b0db67b6af9c608c46d0446fe733e
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:40:34 +0300

    Create Program.cs

---
+++ Basic Syntax - Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int qtySudent = int.Parse(Console.ReadLine());
+            double priceForSabel = double.Parse(Console.ReadLine());
+            double priceForRobe = double.Parse(Console.ReadLine());
+            double priceForBelt = double.Parse(Console.ReadLine());
+
+            double moneySabel = Math.Ceiling(qtySudent * 1.1) * priceForSabel;
+            double moneyRobe = qtySudent * priceForRobe;
+            double moneyBelt = (qtySudent * priceForBelt) - (qtySudent / 6) * priceForBelt;
+
+
+
+            double totalMoney = moneySabel + moneyRobe + moneyBelt;
+
+            if (budget >= totalMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalMoney:f2}lv.");
+
+            }
+            else
+            {
+                Console.WriteLine($"John will need {totalMoney-budget:f2}lv more.");
+            }
+        }
+    }
+}
