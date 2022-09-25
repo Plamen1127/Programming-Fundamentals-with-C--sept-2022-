7c13ad99ed85c108841382cc78f7eeb4702c1655
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-25 23:40:02 +0300

    Create Program.cs

---
+++ Basic Syntax - Exercise/05.Login/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string pass = string.Empty;
+
+            for (int i = username.Length-1; i >= 0; i--)
+            {
+                char curentChar = username[i];
+                pass += curentChar;
+            }
+            string inputPass = Console.ReadLine();
+            int counter = 1;
+            while ( inputPass != pass)
+            {
+                
+                Console.WriteLine($"Incorrect password. Try again.");
+                inputPass = Console.ReadLine();
+                counter += 1;
+                if (counter == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+            }
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
