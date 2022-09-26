92508c7a4a77e0e29ae9469634249818abd7a02e
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-26 23:02:52 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace _01.IntegerOperations
+{
+    using System;
+    using System.Numerics;
+   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+
+            long firstResult = (long)n1 + n2;
+            BigInteger secundResult = (BigInteger)firstResult / n3;
+            BigInteger thirdRsult = secundResult * n4;
+
+            Console.WriteLine(thirdRsult);
+
+
+        }
+    }
+}
