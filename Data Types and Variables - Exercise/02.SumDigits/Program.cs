7c1449743c3b152df96a549556fd91892ff74bd1
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-26 23:03:01 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/02.SumDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace _02.SumDigits
+{
+    using System;
+    using System.Numerics;
+    `
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger num = BigInteger.Parse(Console.ReadLine());
+            BigInteger numCopy = num;
+            BigInteger sum = 0;
+
+            while(numCopy > 0)
+            {
+                BigInteger lastDigit = numCopy % 10;
+                numCopy /= 10;
+                sum += lastDigit;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
