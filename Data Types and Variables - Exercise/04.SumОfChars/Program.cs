8b4ec043f4d892476d1d14670bd8d9c577ec2a36
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-26 23:03:24 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/04.SumОfChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04.SumОfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= n ; i++)
+            {
+                char cha = char.Parse(Console.ReadLine());
+
+                sum += (int)cha;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
