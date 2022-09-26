7cf9aebb508c5eeaf6c6560e019ada7705cacb30
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-26 23:03:15 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/03.Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtyPeople = int.Parse(Console.ReadLine());
+            int capacityElevator = int.Parse(Console.ReadLine());
+
+
+            double course = Math.Ceiling((double)qtyPeople / capacityElevator);
+            Console.WriteLine(course);
+        }
+    }
+}
