b8ad0c5a41451066013c453b28ee16a8fd5593e2
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-28 23:02:57 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/05.PrintPartOfASCII able/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.PrintPartOfASCII_able
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            
+
+            for (int i = n1; i <=n2; i++)
+            {
+                char sumbols = (char)i;
+                Console.Write($"{sumbols} ");
+            }
+        }
+    }
+}
