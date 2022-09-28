e9d9984ae5a6c9ef69b7cc595275943ac91c0750
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-28 23:03:03 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtyInput = int.Parse(Console.ReadLine());
+            int totalLiter = 0;
+            int counterSum = 0;
+
+            for (int i = 1; i <= qtyInput; i++)
+            {
+                
+                int amountOfWater = int.Parse(Console.ReadLine());
+                counterSum += amountOfWater;
+                if (amountOfWater < 255 && counterSum <= 255)
+                {
+                    totalLiter += amountOfWater;
+                }
+                else
+                {
+                    counterSum -= amountOfWater;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(totalLiter);
+        }
+    }
+}
