ae70c833af7c62c999b13f76e33d60b86395dff4
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-28 23:00:36 +0300

    Create Program.cs

---
+++ Data Types and Variables - More Exercise/01.Data ypeFinder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Basic_Syntax__Conditional_Statements_and_Loops___More_Exercise28_._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+
+                else if (double.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+
+                else if (char.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+
+                else if (bool.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
