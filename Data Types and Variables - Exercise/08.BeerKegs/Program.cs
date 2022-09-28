c61de68479b416c5931b6e211bec0bcd2053e377
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-09-28 23:03:07 +0300

    Create Program.cs

---
+++ Data Types and Variables - Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace _08.BeerKegs
+{
+    using System;
+    using System.Numerics;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double volume = 0;
+            string nameType = "";
+            for (int i = 1; i <=n ; i++)
+            {
+                double curentVolume = 0;
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                curentVolume +=  Math.PI * radius * radius * height;
+               
+
+                if (curentVolume > volume)
+                {
+                    volume = curentVolume;
+                    nameType = model;
+                }
+            }
+
+            Console.WriteLine(nameType);
+        }
+    }
+}
