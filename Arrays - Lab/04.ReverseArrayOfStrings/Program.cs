532667f2167009583dd235ea503f0637cf0af5ec
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-01 23:14:11 +0300

    Create Program.cs

---
+++ Arrays - Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+
+            for (int i = elements.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{elements[i]} ");
+            }
+        }
+    }
+}
