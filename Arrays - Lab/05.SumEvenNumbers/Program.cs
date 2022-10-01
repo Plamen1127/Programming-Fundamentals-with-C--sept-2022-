7127d4b98471fb86ab0e5bebf0a5ce9921a563c5
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-01 23:14:15 +0300

    Create Program.cs

---
+++ Arrays - Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nubmers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+            for (int i = 0; i < nubmers.Length; i++)
+            {
+                if ( nubmers[i] % 2 == 0)
+                {
+                    sum += nubmers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
