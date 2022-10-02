6815b0810b74c2f69e66fcc0bf5813681f069d15
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-02 18:28:30 +0300

    Create Program.cs

---
+++ Arrays - Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrays1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] arrays2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < arrays1.Length; i++)
+            {
+                for (int k = i; k == i; k++)
+                {
+                    if (arrays1[i] == arrays2[k])
+                    {
+                        sum += arrays1[i];
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+        }
+    }
+}
