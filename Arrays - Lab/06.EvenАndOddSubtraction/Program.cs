999a6e5421afdcd10b95caa2a3a3af1900e766d4
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-02 18:28:26 +0300

    Create Program.cs

---
+++ Arrays - Lab/06.EvenАndOddSubtraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EvenАndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+            double evenSum = 0;
+            double oddSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+                else if (numbers[i] % 2 != 0)
+                {
+                    oddSum += numbers[i];
+                }
+            }
+
+            Console.WriteLine(evenSum-oddSum);
+        }
+    }
+}
