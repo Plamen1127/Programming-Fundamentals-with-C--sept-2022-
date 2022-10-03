5621d785a1bf0f66e0d10d81e450117ff5ff8605
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-03 23:44:10 +0300

    Create Program.cs

---
+++ Arrays - Exercise/02.CommonElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string[] secundArray = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+
+            for (int i = 0; i < secundArray.Length; i++)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    if (secundArray[i] == firstArray[j])
+                    {
+                        Console.Write($"{secundArray[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
