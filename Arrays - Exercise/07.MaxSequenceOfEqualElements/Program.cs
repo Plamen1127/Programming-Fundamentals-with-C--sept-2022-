bfac0ff1b66010150e9408dd17be6894c07385ea
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-04 23:06:09 +0300

    Create Program.cs

---
+++ Arrays - Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)                         //2 1 1 2 3 3 2 2 2 1
+                               .ToArray();
+            int counter = 0;
+            int winningCounter = 0;
+         
+            int number = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int show = numbers[i];
+                if (numbers[i] == numbers[i + 1])
+                {
+                    counter++;
+                    if (counter > winningCounter)
+                    {
+                        winningCounter = counter;
+                        number = numbers[i];
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+            for (int i = 0; i <= winningCounter; i++)
+            {
+                Console.Write($"{number} ");
+            }
+
+
+
+        }
+    }
+    
+}
