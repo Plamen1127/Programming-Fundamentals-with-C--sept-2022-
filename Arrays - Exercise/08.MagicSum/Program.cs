34ed7df23dac3186c02c8237f3515d26490e6d11
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-04 23:06:13 +0300

    Create Program.cs

---
+++ Arrays - Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    if (array[i]+ array[k] == number)
+                    {
+                        int[] newAraay = { array[i], array[k] };
+                        Console.WriteLine(string.Join(" ", newAraay));
+                    }
+                }
+            }
+        }
+    }
+}
