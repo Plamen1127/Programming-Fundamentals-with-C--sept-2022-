4aaccd3d5cf4581a07c830afab7e16be93e8b02e
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:26:43 +0300

    Create Program.cs

---
+++ Methods - Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <=num; i++)
+            {
+                PrintColums(i); 
+            }
+            for (int i = num - 1; i >= 1; i--)
+            {
+                PrintColums(i);
+            }
+        }
+
+        private static void PrintColums(int i)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
