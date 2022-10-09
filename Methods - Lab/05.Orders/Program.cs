60647063f15db82f4e49fb383cb1819282763e56
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:26:49 +0300

    Create Program.cs

---
+++ Methods - Lab/05.Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int qty = int.Parse(Console.ReadLine());
+            
+                                                                                                                                                                        NewMethod(product, qty);
+
+        }
+
+        private static int NewMethod(string product, int qty)
+        {
+            double price = 0;
+            if (product == "coffee")
+            {
+                price = 1.50;
+            }
+            else if (product == "water")
+            {
+                price = 1.00;
+            }
+            else if (product == "coke")
+            {
+                price = 1.40;
+            }
+            else if (product == "snacks")
+            {
+                price = 2.00;
+            }
+            Console.WriteLine($"{qty * price:f2}");
+            
+        }
+    }
+}
