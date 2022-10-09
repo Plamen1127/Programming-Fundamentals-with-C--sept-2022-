5fe0b325c911dc18512e54a35e946efbf2b23c3a
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:26:37 +0300

    Create Program.cs

---
+++ Methods - Lab/03.Calculations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _03.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case "add":
+                    Add(a, b);
+                    break;
+                case "subtract":
+                    Subtract(a, b);
+                    break;
+                case "multiply":
+                    Multiplay(a, b);
+                    break;
+                case "divide":
+                    Divide(a, b);
+                    break;
+            }
+        }
+
+        private static void Add( int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+        private static void Subtract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        private static void Multiplay(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        private static void Divide(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
