f142a5d84d87cf56735344e1e84ba2760c86856a
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:27:29 +0300

    Create Program.cs

---
+++ Methods - Lab/11.MathOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculation(num1, @operator, num2));
+
+        }
+
+        static double Calculation(int num1, string @operator, int num2)
+        {
+            double result = 0;
+            if (@operator == "/")
+            {
+                result = num1 / num2;
+                
+            }
+
+            else if (@operator == "*")
+            {
+                result = num1 * num2;
+                
+            }
+
+            else if (@operator == "+")
+            {
+                result = num1 + num2;
+                
+            }
+
+            else if (@operator == "-")
+            {
+                result = num1 - num2;
+                
+            }
+            return result;
+        }
+    }
+}
