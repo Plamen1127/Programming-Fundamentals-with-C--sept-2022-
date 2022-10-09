215ca91a6bda7409d31b2ec02f18890af4f65fa5
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:27:22 +0300

    Create Program.cs

---
+++ Methods - Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _10.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            num = Math.Abs(num);
+
+            int totalSum = GetSumOfEvenDigits(num) * GetSumOfOddDigits(num);
+            Console.WriteLine(totalSum);
+
+        }
+        static int GetSumOfEvenDigits(int num)
+        {
+            int sum = 0;
+            int digit = num;
+
+            while (digit > 0)
+            {
+                int currentDigit = digit % 10;
+                if (currentDigit % 2 == 0)
+                {
+                    sum += currentDigit;
+
+                }
+                digit = digit / 10;
+            }
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int num)
+        {
+            int sum = 0;
+            int digit = num;
+
+            while (digit > 0)
+            {
+                int currentDigit = digit % 10;
+                if (currentDigit % 2 != 0)
+                {
+                    sum += currentDigit;
+
+                }
+                digit = digit / 10;
+            }
+            return sum;
+        }
+    }
+}
