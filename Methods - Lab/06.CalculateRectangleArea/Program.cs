1ab85ec97aed2f8e18082e05b6ddc7016d38f64a
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:26:57 +0300

    Create Program.cs

---
+++ Methods - Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int area = CalculateAreaRectangl(num1, num2);
+            Console.WriteLine(area);
+        }
+        static int CalculateAreaRectangl(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
