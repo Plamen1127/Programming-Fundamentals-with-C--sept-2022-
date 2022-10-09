464a2895b5911eb083f79c4a1b680705ad9342dc
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-09 20:26:20 +0300

    Create Program.cs

---
+++ Methods - Lab/01.SignОfIntegerNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SignОfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            NewMethod(n);
+        }
+
+        private static void NewMethod(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive. ");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero. ");
+            }
+        }
+    }
+}
