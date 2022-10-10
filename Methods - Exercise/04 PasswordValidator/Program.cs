7d69917dbbc9088e68e07f486247f67df4be41dc
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-10 21:16:06 +0300

    Create Program.cs

---
+++ Methods - Exercise/04 PasswordValidator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04_PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPass = Console.ReadLine();
+
+
+            ChecksHowManyCharactersThereAre(inputPass);
+
+        }
+
+        static void ChecksHowManyCharactersThereAre(string inputPass)
+        { 
+            int counterChar = 0;
+
+            for (int i = 0; i <= inputPass.Length-1; i++)
+            {
+                counterChar++;
+            }
+            if(counterChar <= 6 || counterChar >= 10)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+        }
+
+    }
+}
