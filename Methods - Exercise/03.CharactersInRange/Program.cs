4c1f530085596ce91a7c3f2724682c5c1789b367
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-10 21:15:57 +0300

    Create Program.cs

---
+++ Methods - Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+
+            PrintAllCharactersBetweenCharacters(firstChar, secondChar);
+            
+        }
+
+        static void PrintAllCharactersBetweenCharacters(char firstChar, char secondChar)
+        {
+
+            int resul = firstChar.CompareTo(secondChar);
+            if ( resul < 0)
+            {
+                for (int i = firstChar+1; i <= secondChar-1; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+            else
+            {
+                for (int i = secondChar + 1; i <= firstChar - 1; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+        }
+    }
+}
