8f28a019f658a1fd429ccb5099813f1e6e9ef95f
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-10 21:15:49 +0300

    Create Program.cs

---
+++ Methods - Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            PrintOutTheNumberOfVolwels(input);
+        }
+
+        static void PrintOutTheNumberOfVolwels(string input)
+        {
+            int counter = 0;
+            for (int i = 0; i <= input.Length-1; i++)
+            {
+                
+                if (input[i] == 'a' || input[i] == 'A' || input[i] == 'e' || input[i] == 'E' || input[i] == 'i' || input[i] == 'I' || input[i] == 'o' || input[i] == 'O' || input[i] == 'u' || input[i] == 'U' || input[i] == 'y' || input[i] == 'Y')
+                {
+                    counter += 1;
+                }
+            }
+            Console.WriteLine(counter);
+
+        }
+    }
+}
