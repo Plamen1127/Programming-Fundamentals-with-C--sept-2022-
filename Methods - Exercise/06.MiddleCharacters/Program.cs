7b90be115872049e1ddc6121c3c07e1b9a254ed3
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-11 23:57:19 +0300

    Create Program.cs

---
+++ Methods - Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(MiddleCharacters(input));
+
+        }
+
+        static string MiddleCharacters(string input)
+        {
+            string midle = string.Empty;
+            if (input.Length % 2 == 0)
+            {
+                midle = input[input.Length / 2].ToString();
+
+            }
+            else
+            {
+                midle = input[input.Length / 2 - 1].ToString() + input[input.Length / 2].ToString();
+            }
+            return midle;
+        }
+    }
+}
