87f5fc51b90a41b61bdf9e5a59ff96e51f0e73d8
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-11 23:57:15 +0300

    Create Program.cs

---
+++ Methods - Exercise/05.AddАndSubtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05.AddАndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = SumOfTheTwoNombers(num1, num2);
+            Console.WriteLine(SubtrakSumFromNum3(sum, num3));
+
+        }
+
+        static int SumOfTheTwoNombers(int num1, int num2)
+        {
+            int sumOfTwoNombers = num1 + num2;
+            return sumOfTwoNombers;
+        }
+
+        static int SubtrakSumFromNum3(int sum, int num3)
+        {
+            int sum2 = sum - num3;
+            return sum2;
+        }
+    }
+}
