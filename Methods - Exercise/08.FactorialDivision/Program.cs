cc894d606867261776e656c3810a7883ff2764ef
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-11 23:57:29 +0300

    Create Program.cs

---
+++ Methods - Exercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08.FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            double result = Facturiel(num1) / Facturiel(num2);
+            Console.WriteLine($"{result:f2}");
+
+        }
+
+        static double Facturiel( double num1)
+        {
+            double sum = 1;
+            for (int i = 1; i <=num1; i++)
+            {
+                sum *= i;
+            }
+            return sum;
+        }
+    }
+}
