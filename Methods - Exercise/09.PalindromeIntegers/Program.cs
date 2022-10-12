633822c2fae4f10f1d6834d7169453fc9687445f
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-12 23:27:57 +0300

    Create Program.cs

---
+++ Methods - Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+         
+            while ( input != "END")
+            {
+                Console.WriteLine(IsNumberPolindrome(input));
+                input = Console.ReadLine();
+            }
+
+
+            
+
+
+
+        }
+        static bool IsNumberPolindrome(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string first = input[i].ToString();
+                string second = input[input.Length - 1 - i].ToString();
+
+                if (first != second)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+
+
+        }
+    }
+}
