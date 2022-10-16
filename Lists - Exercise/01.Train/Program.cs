802fddd9ab24b4db93a98724128613ae2a6872d8
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-16 22:00:20 +0300

    Lists - Exercise
    Problem 1 and 2

---
+++ Lists - Exercise/01.Train/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int maxCapacityOfAWagon = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] inputParams = input.Split();
+
+                if (inputParams[0] == "Add")
+                {
+                    int value = int.Parse(inputParams[1]);
+                    wagons.Add(value);
+                }
+                else
+                {
+                    int value = int.Parse(inputParams[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int sumPassengers = value + wagons[i];
+                        if (sumPassengers <= maxCapacityOfAWagon)
+                        {
+                            wagons.Insert(i, sumPassengers);
+                            wagons.RemoveAt(i + 1);
+                            break;
+                        }
+
+                    }
+                }
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ Lists - Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while(input != "end")
+            {
+                string[] inputParams = input.Split();
+
+                if( inputParams[0] == "Delete")
+                {
+                    int value = int.Parse(inputParams[1]);
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if ( numbers[i]== value)
+                        {
+                            numbers.Remove(value);
+                        }
+                    }
+                }
+                else if ( inputParams[0] == "Insert")
+                {
+                    int element = int.Parse(inputParams[1]);
+                    int value = int.Parse(inputParams[2]);
+                    numbers.Insert(value, element);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
