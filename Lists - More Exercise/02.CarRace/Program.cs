cbce2a9b3a99a52c764b8ffa2321363d4a2d6461
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-10-23 00:24:56 +0300

    List - More Exerise
    Problem 2

---
+++ Lists - More Exercise/02.CarRace/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.CarRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] time = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            double leftPlayer = 0;
+            double rightPlayer = 0;
+
+            for (int i = 0; i < time.Length/2; i++)
+            {
+                leftPlayer += time[i];
+                rightPlayer += time[time.Length-1-i];
+
+                if(time[i] == 0)
+                {
+                    leftPlayer *= 0.8;
+                }
+
+                if (time[time.Length - 1 - i] == 0)
+                {
+                    rightPlayer *= 0.8;
+                }
+            }
+
+            if (leftPlayer < rightPlayer)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftPlayer}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightPlayer}");
+            }
+        }
+    }
+}
