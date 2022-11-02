ee69a1926da6be467a5bee278fee3fbdd99b44ac
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-11-03 00:09:47 +0200

    Objects and Classes - Lab
    Problem 5 and 6

---
+++ Objects and Classes - Lab/05.Students2.0/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] studentProparty = command.Split(' ');
+                string firstName = studentProparty[0];
+                string lastName = studentProparty[1];
+                int age = int.Parse(studentProparty[2]);
+                string town = studentProparty[3];
+
+
+                Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+                if (student != null)
+                {
+                    student.Age = age;
+                    student.Town = town;
+                }
+                else
+                {
+                    student = new Student(firstName, lastName, age, town);
+                    students.Add(student);
+                }
+
+
+                
+            }
+
+            string city = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (city == student.Town)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+
+        }
+
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string town)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Town = town;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+    }
+}
---
+++ Objects and Classes - Lab/06.StoreBoxes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.StoreBoxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split(" ");
+                Box box = new Box
+                {
+                    SerialNumber = tokens[0],
+                    Item = new Item
+                    {
+                        Name = tokens[1],
+                        Price = decimal.Parse(tokens[3])
+                    },
+
+                    ItemQuantity = int.Parse(tokens[2])
+                };
+
+                boxes.Add(box);
+            }
+            List<Box> orderBoxes = boxes.OrderByDescending(box => box.PriceOfBox).ToList();
+            foreach (Box box in orderBoxes)
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:F2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceOfBox:F2}");
+            }
+
+        }
+    }
+    class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceOfBox
+        {
+            get
+            {
+                return ItemQuantity * Item.Price;
+            }
+        }
+    }
+}
