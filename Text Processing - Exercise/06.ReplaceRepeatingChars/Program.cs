447d5adc2f5bb79c54421e3c5a9f5c6c8493abb4
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-11-14 23:19:43 +0200

    Text Processing - Exercise
    Problem 6 and 7

---
+++ Text Processing - Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            StringBuilder newWord = new StringBuilder();
+
+            char curentChar = word[0];
+            newWord.Append(curentChar);
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != curentChar)
+                {
+                    newWord.Append(word[i]);
+                    curentChar = word[i];
+                }
+            }
+
+            Console.WriteLine(newWord); ;
+        }
+    }
+}
---
+++ Text Processing - Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int power = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    power += int.Parse(input[i + 1].ToString());
+                }
+                else if (input[i] != '>' && power > 0)
+                {
+                    input = input.Remove(i, 1);
+                    i--;
+                    power--;
+                }
+            }
+            Console.WriteLine(input);
+
+        }
+    }
+}
