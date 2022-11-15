62de6e0c14a99a456b6b5088b5e8395cca646f2a
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-11-15 23:27:34 +0200

    Text Processing - More Exercise
    Problem 1 and 2

---
+++ Text Processing - More Exercise/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.ExtractPersonInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtyLines = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < qtyLines; i++)
+            {
+                string person = Console.ReadLine();
+                string name = string.Empty;
+                string age = string.Empty;
+                int startName = person.IndexOf('@') + 1;
+                int endName = person.IndexOf('|');
+                int startAge = person.IndexOf('#') + 1;
+                int endAge = person.IndexOf('*');
+
+                for (int k = startName; k < endName; k++)
+                {
+                    name += person[k];
+                }
+                for (int l = startAge; l < endAge; l++)
+                {
+                    age += person[l];
+                }
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
---
+++ Text Processing - More Exercise/02.AsciiSumator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int sum = 0;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar > char1 && currentChar < char2)
+                {
+                    sum += currentChar;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
