f52f6fff633cf76f0f05b112383a955a35cc68b7
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-11-20 23:40:16 +0200

    Regular Expressions - Exercise
    Problem 1

---
+++ Regular Expressions - Exercise/01.Furniture/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^>>(?<furniture>[A-Za-z]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)(.\d+)?$";
+            List<string> furniture = new List<string>();
+
+            Regex regex = new Regex(pattern);
+
+            string input;
+            double totalPrice = 0;
+
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string furnitureName = match.Groups["furniture"].Value;
+                    double price = double.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    totalPrice += price * quantity;
+                    furniture.Add(furnitureName);
+                }
+
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in furniture)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
+        }
+    }
+}
