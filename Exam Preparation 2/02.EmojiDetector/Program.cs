de6d109cf81f4e5aa28f62a3dfb7ca0c017b1942
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-12-03 19:24:00 +0200

    Exam Preparation 2
    Problem 2

---
+++ Exam Preparation 2/02.EmojiDetector/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02.EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int coolThresholt = 1;
+
+            foreach (char ch in input)
+            {
+                if (char.IsDigit(ch))
+                {
+                    int digit = int.Parse(ch.ToString());
+                    coolThresholt *= digit;
+                }
+            }
+            string pattrern = @"((\:\:)|(\*\*))(?<emoji>[A-Z][a-z]{2,})\1";
+
+            Regex regex = new Regex(pattrern);
+            MatchCollection maches = regex.Matches(input);
+
+            Console.WriteLine($"Cool threshold: {coolThresholt}");
+            Console.Write($"{maches.Count} emojis found in the text. ");
+            Console.WriteLine("The cool ones are:");
+
+            foreach (Match match in maches)
+            {
+                string curentEmoji = match;
+                int emojiSimvolsSum = 0;
+
+                foreach (char ch in curentEmoji)
+                {
+                    emojiSimvolsSum += ch;
+                }
+                if (emojiSimvolsSum>=coolThresholt)
+                {
+                    Console.WriteLine($"{match}");
+                }
+            }
+
+
+        }
+    }
+}
