1a2e47663a64ccc9fbf6767f7c0da4a9c78d936b
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-12-05 23:01:09 +0200

    Regular Expressions - More Exercise
    Problem 2

---
+++ Regular Expressions - More Exercise/3.Post Office/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _3.Post_Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "asdasdsadsadasdasdasdasdasdasdasdasdasdas";
+            Console.WriteLine($"Unique symbols used: {message.Distinct().Count()}");
+        }
+    }
+}
