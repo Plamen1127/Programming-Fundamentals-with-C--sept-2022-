3d2860c5c611f2e571c6d4163870a3c8f3073c52
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-12-06 22:06:51 +0200

    Regular Expressions - More Exercise
    problem 2 and 3

---
+++ Regular Expressions - More Exercise/2.RageQuit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2.RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(([^0-9]*)([0-9]+))";
+            string input = Console.ReadLine();
+
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            StringBuilder message = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                string crnMessage = match.Groups[2].Value.ToUpper();
+                int count = int.Parse(match.Groups[3].Value);
+
+                for (int i = 0; i < count; i++)
+                {
+                    message.Append(crnMessage);
+                }
+            }
+
+            Console.WriteLine($"Unique symbols used: {message.ToString().Distinct().Count()}");
+            Console.WriteLine(message);
+
+        }
+    }
+}
