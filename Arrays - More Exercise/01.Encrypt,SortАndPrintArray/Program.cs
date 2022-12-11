b3748b5d4ac6c1ff2249d798714edae3b785bb06
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-12-11 20:25:56 +0200

    Arrays - More Exercise
    Problem 1 and 2

---
+++ Arrays - More Exercise/01.Encrypt,SortАndPrintArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] vowel = new char[] { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' };
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int sum = 0;
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    char curentLeter = input[k];
+
+                    if (vowel.Contains(curentLeter))
+                    {
+                        sum += curentLeter * input.Length;
+                    }
+                    else
+                    {
+                        sum += curentLeter / input.Length;
+                    }
+                }
+
+                numbers[i] = sum;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, numbers.OrderBy(x => x)));
+            //  Array.Sort(numbers);
+            // foreach (var item in numbers)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+        }
+    }
+}
