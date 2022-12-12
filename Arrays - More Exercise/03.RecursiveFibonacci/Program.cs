2f71cdff10f2f5070c52f5be266264bd2a3c51c1
Plamen Savov <111393280+Plamen1127@users.noreply.github.com>
2022-12-12 21:21:58 +0200

    Arrays - More Exercise
    Problem 3

---
+++ Arrays - More Exercise/03.RecursiveFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿uusing System;
+
+namespace _03.RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] getFibonacci = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 1;
+                if (i > 1)
+                {
+                    sum = getFibonacci[i - 1] + getFibonacci[i - 2];
+                }
+
+                getFibonacci[i] = sum;
+            }
+
+            Console.WriteLine(getFibonacci[n - 1]);
+        }
+    }
+}
